56cf3235da3d7cca0648722365688e0e1720b708
Anthony <schubeal@gmail.com>
2016-01-30 14:09:36 -0700

    version scheme
      * 3 digit, starting with 0.0.1a1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-pastebinapp',
-    version='0.1',
+    version='0.0.1a1',
     packages=['pastebinapp'],
     include_package_data=True,
     license='MIT License',
